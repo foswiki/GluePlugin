d9910da451960d80db98e7311b0b2c811326bdfb
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2014-04-23 11:18:32 +0000

    Item12864: removed $Rev$ from VERSION tag
    git-svn-id: http://svn.foswiki.org/trunk/GluePlugin@17567 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GluePlugin.pm
+++ lib/Foswiki/Plugins/GluePlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2005-2009 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2005-2014 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,17 +16,14 @@
 # =========================
 package Foswiki::Plugins::GluePlugin;
 use strict;
+use warnings;
 
 # =========================
-use vars qw(
-        $VERSION $RELEASE 
-	$NO_PREFS_IN_TOPIC $SHORTDESCRIPTION
-    );
 
-$VERSION = '$Rev$';
-$RELEASE = '2.20';
-$NO_PREFS_IN_TOPIC = 1;
-$SHORTDESCRIPTION = 'Enable markup to span multiple lines';
+our $VERSION = '2.20';
+our $RELEASE = '2.20';
+our $NO_PREFS_IN_TOPIC = 1;
+our $SHORTDESCRIPTION = 'Enable markup to span multiple lines';
 
 # =========================
 sub initPlugin { 
